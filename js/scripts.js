37e2ec63649f71c885f519eee0ccd4378df498a4
Karol <karolpoplawski@gmail.com>
2016-10-15 16:24:15 +0200

    sample jquery slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,39 @@
- $('span').each(function(index, element) { 
-  if(index % 2 == 0) { 
-    $(element).css('color', 'red'); 
-  }; 
+$(function(){
+  
+  var carouselList = $('#carousel ul');
+  //change slide every 3 seconds 
+  var interval = setInterval(changeSlide, 3000); 
+  
+  function moveFirstSlide(){
+    var firstItem = carouselList.find("li:first");  
+    var lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem);
+    carouselList.css({marginLeft:0});
+  };
+  
+    function moveLastSlide(){
+    var firstItem = carouselList.find("li:first");  
+    var lastItem = carouselList.find("li:last");
+    lastItem.before(firstItem);
+    carouselList.css({marginLeft:0});
+  };
+   
+    function changeSlide(){
+      carouselList.animate({'marginLeft':-700}, 1000, moveFirstSlide);
+  }
+  
+    function reverseSlide(){
+      carouselList.animate({'marginLeft':700}, 1000, moveLastSlide);
+  } 
+  
+    $('#chevron-right').click(changeSlide);
+    //$('#chevron-left').click(reverseSlide);
+
+	$('#chevron-left').on('click', function () {
+	  reverseSlide();
+	  clearInterval(interval);
+	  interval = setInterval(changeSlide, 3000);
+	});
 });  
 
-/* $("span:odd").css('color','red'); */
-
-var paragraphs = $('p'); 
-paragraphs.each(function(index, element) { 
-  
-  var button = '<button class="btn" data-tmp="' + index + '">Click me</button>' 
-  $(element).append(button) 
-  
-});
-
-$("button").click(function(){ 
-    alert($(this).attr("data-tmp")); 
-});
+ 
